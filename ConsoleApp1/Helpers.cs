8b02b8e3c72d0e1205d8e186ccce7d087eb717ae
Santiago Sica <santisica29@gmail.com>
2024-06-21 12:56:02 -0300

    use oop and separation of concerns to make the code cleaner

---
+++ ConsoleApp1/Helpers.cs
@@ -0,0 +1,64 @@
+﻿namespace ConsoleApp1
+{
+    internal class Helpers
+    {
+        static List<string> games = new();
+        internal static string GetName()
+        {
+            Console.WriteLine("Please type your name");
+            var name = Console.ReadLine();
+            return name;
+        }
+
+
+       internal static void ViewPrevGames()
+        {
+            if (games.Count == 0)
+            {
+                Console.WriteLine("There are no games recorded.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Games History");
+            Console.WriteLine("--------------------------");
+            foreach (var game in games)
+            {
+                Console.WriteLine($"{game}");
+            }
+
+            Console.WriteLine("--------------------------\n");
+            Console.WriteLine("Press any key to go back to menu");
+            Console.ReadLine();
+
+        }
+
+        internal static void AddToHistory(int score, string typeGame)
+        {
+            games.Add($"{DateTime.Now} - {typeGame}: Score = {score}");
+            // char.ToUpper(typeGame[0])+typeGame.Substring(1) makes typegame first letter string uppercase
+        }
+
+
+        internal static int[] GetDivisionNumbers()
+        {
+            var random = new Random();
+            var firstNumber = random.Next(0, 99);
+            var secondNumber = random.Next(0, 99);
+
+            var result = new int[2];
+
+            while (firstNumber % secondNumber != 0)
+            {
+                firstNumber = random.Next(1, 99);
+                secondNumber = random.Next(1, 99);
+            }
+
+            result[0] = firstNumber;
+            result[1] = secondNumber;
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Menu.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp1
+{
+    public class Menu
+    {
+        GameEngine engine = new();
+        Helpers helpers = new Helpers();
+        public void ShowMenu(string name, DateTime date)
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine($"Hello {name}. It's {date}. This is your math's game.");
+            Console.WriteLine("\n");
+            Console.ReadLine();
+
+            var isGameOn = true;
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
+                             V - View previous games
+                             A - Addition
+                             S - Subtraction
+                             M - Multiplication
+                             D - Division
+                             Q - Quit the program");
+                Console.WriteLine("---------------------------------------------");
+
+                var gameSelected = Console.ReadLine();
+
+                switch (gameSelected.Trim().ToLower())
+                {
+                    case "v":
+                        Helpers.ViewPrevGames();
+                        break;
+                    case "a":
+                        engine.AdditionGame("Addition game");
+                        break;
+                    case "s":
+                        engine.SubtractionGame("Subtraction game");
+                        break;
+                    case "m":
+                        engine.MultiplicationGame("Multiplication game");
+                        break;
+                    case "d":
+                        engine.DivisionGame("Division game");
+                        break;
+                    case "q":
+                        Console.WriteLine("Goodbye");
+                        isGameOn = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (isGameOn);
+        }
+
+    }
+}
